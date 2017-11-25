b73221b7076d311b434e378d4164e1f19484477a
Patrick Arminio <patrick.arminio@gmail.com>
2017-11-25 23:30:37 +0000

    Add implementation for Expr, Call, Str and Name

--- src/printer-python.js
+++ src/printer-python.js
@@ -4,10 +4,10 @@ const docBuilders = require("./doc-builders");
 const concat = docBuilders.concat;
 const join = docBuilders.join;
 const hardline = docBuilders.hardline;
-// const line = docBuilders.line;
+const line = docBuilders.line;
 // const softline = docBuilders.softline;
 // const group = docBuilders.group;
-// const indent = docBuilders.indent;
+const indent = docBuilders.indent;
 // const ifBreak = docBuilders.ifBreak;
 
 function genericPrint(path, options, print) {
@@ -29,7 +29,35 @@ function genericPrint(path, options, print) {
     }
 
     case "FunctionDef": {
-      return "def something: pass";
+      return concat([
+        "def ",
+        path.call(print, "name"),
+        concat(["(", path.call(print, "args"), ")"]),
+        ":",
+        indent(concat([line, concat(path.map(print, "body"))]))
+      ]);
+    }
+
+    case "arguments": {
+      // TODO: default args, *args, **kwargs,
+      // keyword only arguments
+      return join(", ", n.args.map(a => a.arg));
+    }
+
+    case "Expr": {
+      return path.call(print, "value");
+    }
+
+    case "Call": {
+      return concat([n.func.id, "(", join(", ", path.map(print, "args")), ")"]);
+    }
+
+    case "Str": {
+      return `"${n.s}"`;
+    }
+
+    case "Name": {
+      return n.id;
     }
 
     default:
