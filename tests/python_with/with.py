935d08d0c8a65a573bcc3c75cdf2eb160db14a2b
Danny Weinberg <danny@dropbox.com>
2018-03-02 10:15:38 -0800

    Handle Python 2 `with` blocks and wrapping of `with` items
    This diff handles Python 2's de-sugaring of a single `with` block's items into multiple nested AST nodes and is able to distinguish that case and reproduce accurate output based on the input code. It also handles wrapping long `with` blocks with many items.
    
    Fixes https://github.com/prettier/plugin-python/issues/51

--- tests/python_with/with.py
+++ tests/python_with/with.py
@@ -6,3 +6,12 @@ with open('tmp/index.html'):
 
 with A() as X, B() as Y, C() as Z:
     do_something()
+
+with A() as X:
+    with B() as Y:
+        with C() as Z:
+            do_something()
+
+with long_context_manager_1_aaaaaa, long_context_manager_1_aaaaaa, long_context_manager_1_aaaaaa: pass
+
+with long_context_manager_1_aaaaaa, long_context_manager_1_aaaaaa: long_function_name()
