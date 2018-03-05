c4b7f485c8a521182dd817b8568660a40403c045
Ryan Ashcraft <ryan@ryanashcraft.me>
2018-03-04 17:38:47 -0800

    Fix printing multiple ifs in comprehension nodes
    Fixes #18.

--- tests/python_dict_comp/dict_comp.py
+++ tests/python_dict_comp/dict_comp.py
@@ -1,5 +1,7 @@
 {x: 0 for x in range(100)}
 {x: 0 for x in range(100) if x % 2 == 0}
+{i: 0 for j in k if j%2 == 0 if j*2 < 20 for i in j if i%2==0}
+{my_long_variable_i: 0 for my_long_variable_j in my_long_variable_k if my_long_variable_j%2 == 0 if my_long_variable_j*2 < 20 for my_long_variable_i in my_long_variable_j if my_long_variable_i%2==0}
 
 my_long_variable_aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa = {thing: 1 for thing in things}
 
