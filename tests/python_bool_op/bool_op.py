3f5441265fc3937d991f6796ebabef99155fd4cc
Ryan Ashcraft <ryan@ryanashcraft.me>
2018-03-04 18:37:42 -0800

    Use escaped lines for binary and bool ops

--- tests/python_bool_op/bool_op.py
+++ tests/python_bool_op/bool_op.py
@@ -1,2 +1,4 @@
 data.retweeted or 'RT @' in data.text
 data.retweeted and 'RT @' in data.text
+
+my_long_variable_i and my_long_variable_j and 'LONG_STRING' in my_long_variable_i.my_long_attribute_k
