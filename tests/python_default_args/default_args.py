a2a9b8cf777081b2d595eced34f4cad6648ab419
Ryan Ashcraft <ryan@ryanashcraft.me>
2018-05-04 04:35:28 -0700

    Multiline default args ordering fix (#78)
    * Add failing test for args defaults ordering bug
    
    * Fix default args ordering bug

--- tests/python_default_args/default_args.py
+++ tests/python_default_args/default_args.py
@@ -1,2 +1,7 @@
 def hello(x, a=123, b = 456):
     print("hello world", a)
+
+
+def my_func_with_many_args(self, var_a, var_b, var_c, var_d, var_e=None, var_f=None,
+                        var_g=None, var_h=None, var_i=None, var_j=None):
+    pass
