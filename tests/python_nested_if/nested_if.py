28ef42b26c6b3a091bd602d42cf9502d385cfbcc
Patrick Arminio <patrick.arminio@gmail.com>
2018-05-10 22:47:05 -0700

    Fix nested ifs (#85)
    * Fix nested if printing
    
    * Add additional nested if test

--- tests/python_nested_if/nested_if.py
+++ tests/python_nested_if/nested_if.py
@@ -7,3 +7,15 @@ elif x == 12:
     print('oh')
 else:
     print(123)
+
+if foo:
+    foo()
+elif bar:
+    bar()
+else:
+    if baz:
+        baz()
+    elif garply:
+        garply()
+    else:
+        qux()
